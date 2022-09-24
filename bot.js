d22e75500e22e0de848c359ebf13fe983463ea52
eric <e.salberg213@gmail.com>
2022-09-24 00:29:13 -0400

    added voice recognition

--- bot.js
+++ bot.js
@@ -2,8 +2,10 @@ const fs = require('node:fs')
 const path = require('node:path')
 const { token } = require('./config.json');
 const { Client, Collection, IntentsBitField } = require('discord.js');
+const { joinVoiceChannel } = require("@discordjs/voice");
+const { addSpeechEvent } = require("discord-speech-recognition");
 const client = new Client({
-    intents: [IntentsBitField.Flags.Guilds, IntentsBitField.Flags.GuildMessages, IntentsBitField.Flags.MessageContent]
+    intents: [IntentsBitField.Flags.Guilds, IntentsBitField.Flags.GuildMessages, IntentsBitField.Flags.MessageContent, IntentsBitField.Flags.GuildVoiceStates]
 });
 
 client.commands = new Collection();
@@ -16,11 +18,22 @@ for (const file of commandFiles) {
     client.commands.set(command.data.name, command);
 }
 
-//const sadWords = ["sad", "depressed", "unhappy", "angry", "miserable", "tilt", "smurf"];
-//const encouragementMsg = "keep it pma, keep it bsj";
+const sadWords = ["sad", "depressed", "unhappy", "angry", "miserable", "tilt", "smurf", "f***", "s***", "damn", "kill"];
+const encouragementMsg = "keep it pma, keep it bsj";
+
+addSpeechEvent(client);
 
 client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`) 
+    const voiceChannel = client.channels.cache.get('290178979900555275')
+    if (voiceChannel) {
+      joinVoiceChannel({
+        channelId: voiceChannel.id,
+        guildId: voiceChannel.guild.id,
+        adapterCreator: voiceChannel.guild.voiceAdapterCreator,
+        selfDeaf: false,
+      });
+    }
 });
 
 client.on('interactionCreate', async interaction => {
@@ -37,5 +50,23 @@ client.on('interactionCreate', async interaction => {
         await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
     }
 });
+
+client.on('messageCreate', msg => {
+    if (sadWords.some(word => msg.content.includes(word))) {
+        msg.reply(encouragementMsg);
+    }
+});
+  
+  client.on("speech", (msg) => {
+    // If bot didn't recognize speech, content will be empty
+    console.log(msg.content);
+    if (!msg.content) return;
+
+    //client.channels.cache.get('1023035230250082314').send(`${msg.author.tag} said: ${msg.content}`)
+    if (sadWords.some(word => msg.content.includes(word))) {
+        client.channels.cache.get('1023035230250082314').send(`hey ${msg.author}! ${encouragementMsg}`)
+    }
+    //msg.author.send(msg.content);
+  });
 
 client.login(token);
